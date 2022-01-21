f38a549e62e61cdb1ae162986b8b28dc0a39dbe1
kevert14 <kevert14@byu.edu>
2022-01-20 17:41:14 -0700

    mission2

---
+++ mission2/Models/CalcModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mission2.Models
+{
+    public class CalcModel
+    {
+        [Range(1, 100)]
+        public float assign { get; set; }
+
+        [Range(1, 100)]
+        public float grp_proj { get; set; }
+
+        [Range(1, 100)]
+        public float quiz { get; set; }
+
+        [Range(1, 100)]
+        public float exam { get; set; }
+
+        [Range(1, 100)]
+        public float inte { get; set; }
+
+    }
+}
